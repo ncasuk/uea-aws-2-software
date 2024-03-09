45225f3d088e134c9fc510d6fb617f1e3f7f6d26
Joshua Hampton <joshua.hampton@live.co.uk>
2024-03-09 11:28:13 +0000

    Update product version

--- process_data.sh
+++ process_data.sh
@@ -2,8 +2,8 @@
 
 which_python=/home/users/ncasit/miniconda3/envs/netcdf_311/bin/python
 input_file=/gws/pw/j07/ncas_obs_vol1/wao/processing/uea-aws-2/uea-aws-2_growing.csv
-output_loc=/gws/pw/j07/ncas_obs_vol1/wao/processing/uea-aws-2/20180501_longterm
-product_version=2.0
+output_loc=/gws/pw/j07/ncas_obs_vol1/wao/processing/uea-aws-2/20180515_longterm
+product_version=1.0
 year=2023
 
 ${which_python} process_data.py ${input_file} ${output_loc} ${product_version} ${year}
